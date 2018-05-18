e8df5ec6651858e636b8c88b5d915d7af8bed2e7
Pasquale Gabriel <pasqualeunq@gmail.com>
2018-05-18 13:38:11 -0300

    Se agrego exploreDungeon

--- grails-app/services/maguitograils/PersonajeService.groovy
+++ grails-app/services/maguitograils/PersonajeService.groovy
@@ -31,27 +31,14 @@ class PersonajeService {
     }
 
     def exploreDungeon(Fighter player, Dungeon dungeon) {
-        def dungeonResult = dungeon.monsters.collect {
-            combat(player, it)
-        }
+        def dungeonResult    = dungeon.monsters.collect{combat(player, it)}
         def expeditionResult = new ExpeditionResult(dungeonResult)
-        if(expeditionResult.isTheWinner(player)){
-            dungeon.giveVictory(player)
-        } else {
-            dungeon.getVictory(player)
-        }
+        if(expeditionResult.isTheWinner(player)) dungeon.giveVictory(player)
+        else dungeon.getVictory(player)
         updatePersonaje(player)
         dungeon.save()
         expeditionResult
-    }/*
-    explorarDugeon(player1, combatiente):ResultadoDeExpedicion​ Envia un
-personaje a conquistar un dungeon. El personaje deberá combatir y vencer a
-todos los monstruos de ese dungeon. Si lo hace, se queda con t_odo el oro e
-items de los monstruos y de la tesorería del dungeon. Si pierde, el dungeon se
-queda con todos los items y oro del personaje y los agrega a la tesorería. El
-objeto resultante ResultadoDeExpedicion informará no sólo quien ganó, sino
-todos los resultados de los combates llevados a cabo.
-     */
+    }
 
     def loadAllPersonajes() {
         Personaje.list()
